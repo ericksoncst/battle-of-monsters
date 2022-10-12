e554eb2fdd2a0c6b71befc01b9ae0754eeb33ee2
Thiago Galvani <tgalvani@fullstacklabs.co>
2022-10-12 14:19:35 -0300

    feat: adding the app component

---
+++ src/App.styled.tsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/native';
+import { View } from 'react-native';
+import { colors } from './constants/colors';
+
+export const AppContainer = styled(View)`
+  background: ${colors.lightGray};
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  padding-top: 49px;
+`;
